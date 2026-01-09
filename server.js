1619c7be2978c0b94dd2f67e856a6c765545c611
Innay <id.inna22@gmail.com>
2026-01-09 16:56:15 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 
+const db = require('./db');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
